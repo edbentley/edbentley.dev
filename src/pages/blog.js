291d658f8f168d40512369018eed01d70c541ba8
Ed Bentley <15923595+edbentley@users.noreply.github.com>
2020-04-01 16:22:54 +0100

    Update blog formatting

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -28,27 +28,42 @@ const BlogPage = () => {
   return (
     <main className="app-width" style={{ marginTop: "8px" }}>
       <SEO title="Blog" />
-      {devPosts.map(({ node: { article: post } }) => (
-        <div key={post.id} style={{ margin: "16px 0" }}>
-          <a href={post.url} className="hover">
-            <div
-              className="flex"
-              style={{
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <h2 className="m0">{post.title}</h2>
-              <p className="m0" style={{ color: "grey" }}>
-                {formatDate(post.published_at)}
+      {[...devPosts]
+        .sort(
+          ({ node: { article: postA } }, { node: { article: postB } }) =>
+            -postA.published_at.localeCompare(postB.published_at)
+        )
+        .map(({ node: { article: post } }) => (
+          <div key={post.id} style={{ margin: "16px 0 32px 0" }}>
+            <a href={post.url} className="hover">
+              <div
+                className="flex"
+                style={{
+                  justifyContent: "space-between",
+                  alignItems: "flex-start",
+                }}
+              >
+                <h2 className="m0" style={{ marginRight: 8 }}>
+                  {post.title}
+                </h2>
+                <p
+                  className="m0"
+                  style={{
+                    color: "grey",
+                    textAlign: "end",
+                    flexShrink: 0,
+                    paddingTop: 8,
+                  }}
+                >
+                  {formatDate(post.published_at)}
+                </p>
+              </div>
+              <p className="m0" style={{ color: "grey", marginTop: 8 }}>
+                {post.description}
               </p>
-            </div>
-            <p className="m0" style={{ color: "grey", marginTop: 8 }}>
-              {post.description}
-            </p>
-          </a>
-        </div>
-      ))}
+            </a>
+          </div>
+        ))}
     </main>
   )
 }
